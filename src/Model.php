bf36665cdc945603fb47c432e5913a0fd56985a5
Michael Slowik <slowik.michael@gmail.com>
2019-07-03 18:11:08 -0500

    use all attributes during update

--- src/Model.php
+++ src/Model.php
@@ -742,7 +742,7 @@ abstract class Model implements ArrayAccess, Arrayable, Jsonable, JsonSerializab
         $dirty = $this->getDirty();
 
         if (count($dirty) > 0) {
-            $this->setKeysForSaveQuery($query)->update($dirty);
+            $this->setKeysForSaveQuery($query)->update($this->attributes);
 
             $this->syncChanges();
 
